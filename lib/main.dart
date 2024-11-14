72ab75188bc0f40b087c53994696dc73050d0a9d
김우빈 <oobin765@gmail.com>
2024-11-14 16:10:21 +0900

    firebase fireStone 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,32 @@
 import 'package:flutter/material.dart';
 import 'views/morph_calculator_view.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  try {
+    if (kIsWeb) {
+      await Firebase.initializeApp(
+        options: const FirebaseOptions(
+          apiKey: "AIzaSyBOFSlP1DkVAkycAVjQLOUTvVMevPzCOPU",
+          authDomain: "rep-f-8b018.firebaseapp.com",
+          projectId: "rep-f-8b018",
+          storageBucket: "rep-f-8b018.appspot.com",
+          messagingSenderId: "300527337363",
+          appId: "1:300527337363:web:cbfef119f8a6c8e8b7f266",
+          measurementId: "G-RENHEJS44F",
+        ),
+      );
+    } else {
+      await Firebase.initializeApp();
+    }
+    print('Firebase 초기화 성공');
+  } catch (e) {
+    print('Firebase 초기화 실패: $e');
+  }
+
   runApp(const MyApp());
 }
 
