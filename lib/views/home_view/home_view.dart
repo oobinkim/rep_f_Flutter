da56715e5747da64f5da3b4955c5554cd3c1a674
김우빈 <oobin765@gmail.com>
2024-12-08 15:48:30 +0900

    그리드버튼 이미지와 아이콘 둘다 지원하도록 수정, 그리드버튼 간격수정, 개체추가 버튼 디테일수정

--- lib/views/home_view/home_view.dart
+++ lib/views/home_view/home_view.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import '../../share/app_color.dart';
-import '../../widgets/grid_button.dart';
+import '../../widgets/grid_button.dart'; // GridButton을 임포트
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  int _currentIndex = 0; // 현재 선택된 탭의 인덱스
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.black, // 배경색
+      backgroundColor: AppColors.black,
       appBar: AppBar(
         backgroundColor: AppColors.black,
         elevation: 0,
@@ -28,17 +35,16 @@ class HomeView extends StatelessWidget {
           ],
         ),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          // 이전, 오늘, 다음 버튼 영역
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: Row(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            // 이전, 오늘, 다음 버튼 영역
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 IconButton(
-                  icon: Icon(Icons.arrow_back_ios, color: Colors.white),
+                  icon: Icon(Icons.arrow_back_ios, color: AppColors.white),
                   onPressed: () {
                     print("이전 버튼 눌림");
                   },
@@ -48,94 +54,125 @@ class HomeView extends StatelessWidget {
                     print("오늘 드롭다운 눌림");
                   },
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Icon(Icons.calendar_today, color: Colors.white, size: 16),
-                      SizedBox(width: 8),
+                      Icon(Icons.calendar_today, color: AppColors.white, size: 16),
+                      SizedBox(width: 4),
                       Text(
                         "오늘",
                         style: TextStyle(
-                          color: Colors.white,
+                          color: AppColors.white,
                           fontWeight: FontWeight.w500,
+                          fontSize: 16,
                         ),
                       ),
-                      Icon(Icons.arrow_drop_down, color: Colors.white),
+                      Icon(Icons.arrow_drop_down, color: AppColors.white),
                     ],
                   ),
                 ),
                 IconButton(
-                  icon: Icon(Icons.arrow_forward_ios, color: Colors.white),
+                  icon: Icon(Icons.arrow_forward_ios, color: AppColors.white),
                   onPressed: () {
                     print("다음 버튼 눌림");
                   },
                 ),
               ],
             ),
-          ),
-          SizedBox(height: 16),
-          GestureDetector(
-            onTap: () {
-              print("개체 등록 버튼 눌림");
-            },
-            child: Container(
-              padding: EdgeInsets.all(16), // 내부 여백
-              decoration: BoxDecoration(
-                color:AppColors.darkGray, // 배경색 지정
-                borderRadius: BorderRadius.circular(12), // 둥근 모서리
+            SizedBox(height: 16),
+            // 개체 등록 버튼
+            GestureDetector(
+              onTap: () {
+                print("개체 등록 버튼 눌림");
+              },
+              child: Container(
+                width: double.infinity,
+                height: 200, // 세로 길이를 늘림
+                padding: EdgeInsets.all(24),
+                decoration: BoxDecoration(
+                  color: AppColors.darkGray,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.add_circle, color: AppColors.white, size: 40),
+                    SizedBox(height: 8),
+                    Text(
+                      "개체를 먼저 등록해주세요",
+                      style: TextStyle(
+                        color: AppColors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              child: Column(
+            ),
+            SizedBox(height: 32),
+            // Grid 버튼 영역
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 2,
+                mainAxisSpacing: 16,
+                crossAxisSpacing: 16,
                 children: [
-                  Icon(Icons.add_circle, color: Colors.white, size: 40),
-                  SizedBox(height: 8),
-                  Text(
-                    "개체를 먼저 등록해주세요",
-                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  GridButton(
+                    label: "물 줬어요",
+                    icon: Icons.water_drop,
+                    isSelected: false,
+                    onTap: () {
+                      print("물 줬어요 버튼 눌림");
+                    },
+                  ),
+                  GridButton(
+                    label: "먹이 줬어요",
+                    icon: Icons.fastfood,
+                    isSelected: false,
+                    onTap: () {
+                      print("먹이 줬어요 버튼 눌림");
+                    },
+                  ),
+                  GridButton(
+                    label: "메이팅 했어요",
+                    icon: Icons.favorite,
+                    isSelected: false,
+                    onTap: () {
+                      print("메이팅 했어요 버튼 눌림");
+                    },
+                  ),
+                  GridButton(
+                    label: "배변 했어요",
+                    icon: Icons.pets,
+                    isSelected: false,
+                    onTap: () {
+                      print("배변 했어요 버튼 눌림");
+                    },
+                  ),
+                  GridButton(
+                    label: "탈피 했어요",
+                    icon: Icons.change_circle,
+                    isSelected: false,
+                    onTap: () {
+                      print("탈피 했어요 버튼 눌림");
+                    },
                   ),
                 ],
               ),
             ),
-          ),
-          SizedBox(height: 32),
-          Expanded(
-            child: GridView.count(
-              shrinkWrap: true,
-              crossAxisCount: 2,
-              mainAxisSpacing: 16,
-              crossAxisSpacing: 16,
-              children: [
-                GridButton(
-                  label: "물 줬어요",
-                  icon: Icons.water_drop,
-                  isSelected: false,
-                  onTap: () => print("물 줬어요 버튼 눌림"),
-                ),
-                GridButton(
-                  label: "밥 줬어요",
-                  icon: Icons.fastfood,
-                  isSelected: false,
-                  onTap: () => print("밥 줬어요 버튼 눌림"),
-                ),
-                GridButton(
-                  label: "메이팅 했어요",
-                  icon: Icons.favorite,
-                  isSelected: false,
-                  onTap: () => print("메이팅 했어요 버튼 눌림"),
-                ),
-                GridButton(
-                  label: "배변 했어요",
-                  icon: Icons.pets,
-                  isSelected: false,
-                  onTap: () => print("배변 했어요 버튼 눌림"),
-                ),
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.black,
-        selectedItemColor: Colors.white,
-        unselectedItemColor: Colors.grey,
+        type: BottomNavigationBarType.fixed, // 고정 타입 설정
+        backgroundColor: AppColors.black, // 배경색 검정으로 설정
+        selectedItemColor: AppColors.white, // 선택된 아이템 색상
+        unselectedItemColor: AppColors.lightGray, // 선택되지 않은 아이템 색상
+        currentIndex: _currentIndex, // 현재 선택된 탭의 인덱스
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index; // 선택된 인덱스를 업데이트
+          });
+        },
         items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
--- lib/widgets/grid_button.dart
+++ lib/widgets/grid_button.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import '../../share/app_color.dart';
 
 class GridButton extends StatelessWidget {
   final String label;
-  final IconData icon;
+  final dynamic icon; // IconData 또는 Widget 타입 모두 지원
   final bool isSelected;
   final VoidCallback onTap;
 
@@ -17,40 +16,48 @@ class GridButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: GestureDetector(
-        onTap: onTap,
-        child: Container(
-          height: 180,
-          decoration: BoxDecoration(
-            color: isSelected ? AppColors.purposeButtonGrey : AppColors.darkGray,
-            borderRadius: BorderRadius.circular(12),
-            border: Border.all(
-              color: isSelected ? AppColors.lightGreen : Colors.transparent,
-              width: 2,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          color: isSelected ? Colors.grey[700] : Colors.grey[800],
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(
+            color: isSelected ? Colors.green : Colors.transparent,
+            width: 2,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _buildIcon(), // 아이콘 또는 위젯을 렌더링
+            SizedBox(height: 16),
+            Text(
+              label,
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+                color: Colors.white,
+              ),
             ),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                icon,
-                size: 36,
-                color: AppColors.lightGray,
-              ),
-              SizedBox(height: 16),
-              Text(
-                label,
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500,
-                  color: AppColors.white,
-                ),
-              ),
-            ],
-          ),
+          ],
         ),
       ),
     );
+  }
+
+  Widget _buildIcon() {
+    // 아이콘이 IconData 타입이면 Icon 위젯 생성, 아니면 그대로 반환
+    if (icon is IconData) {
+      return Icon(
+        icon,
+        size: 36,
+        color: Colors.white,
+      );
+    } else if (icon is Widget) {
+      return icon; // 커스텀 위젯 반환 (Image, Svg 등)
+    } else {
+      throw ArgumentError('icon must be either IconData or a Widget');
+    }
   }
 }
